3c86ce946b2ea56e20e5c9f250844182f001204a
Tushar Karkera <104358692+TusharKarkera22@users.noreply.github.com>
2024-01-28 11:35:13 +0530

    Float

--- frontend/src/components/Maincomponent/Main.jsx
+++ frontend/src/components/Maincomponent/Main.jsx
@@ -3,7 +3,7 @@ import Button from '../Button'
 const Main = () => {
   return (
     <div className='flex flex-col  justify-center items-center'>
-      <img className='mt-8' src='/Op.png' width={'400px'} alt='Op' />
+      <img className='mt-8 floating-avatar' src='/Op.png' width={'400px'} alt='Op' />
       <h1 className='font-contax text-[120px]'>Haven</h1>
       <p className='font-contax w-[1300px] text-center text-[#00000080]'>
   Welcome to the First Stable Fund Distribution platform on Optimism. We're transforming Crowdfunding, providing a secure space for Innovative projects. Join us for Decentralized and Transparent Fundraising. Empower your ideas, Join the Movement, and experience Trustless Fundraising with Optimism.
